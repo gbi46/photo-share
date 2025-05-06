763791977457b72784948a6e001868445a65826f
ivcoder <vank300828@gmail.com>
2025-05-06 17:54:10 +0200

    add endpoint for user acc upd

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
-from src.core.dependencies import can_view_account, require_role
+from src.core.dependencies import can_view_account, can_update_account, require_role
 from src.repositories.user import UserRepository
-from src.schemas.user import UserAccountResponse, UserProfileResponse
+from src.schemas.user import UserAccountResponse, UserProfileResponse, UserUpdateRequest
 from src.services.user import UserService
 
 router = APIRouter(prefix='/users', tags=['users'])
@@ -23,3 +23,18 @@ async def get_user_account(
     service = UserService(UserRepository(db))
     
     return await service.get_account(account.id)
+
+@router.put("/{account_id}", response_model=UserAccountResponse)
+async def update_account(
+    user = require_role('user'),
+    update_data: UserUpdateRequest = Body(...),
+    account = can_update_account(), 
+    db: AsyncSession = Depends(get_db)
+):
+    service = UserService(UserRepository(db))
+    account = await service.get_account(account.id)
+
+    if account is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    
+    return await service.update_account(account.id, update_data)
--- src/services/user.py
+++ src/services/user.py
@@ -1,5 +1,5 @@
 from src.repositories.user import UserRepository
-from src.schemas.user import UserProfileResponse
+from src.schemas.user import UserAccountResponse, UserProfileResponse, UserUpdateRequest
 from uuid import UUID
 
 class UserService:
@@ -10,5 +10,12 @@ class UserService:
     async def get_profile_by_username(self, username: str) -> UserProfileResponse:
         return await self.user_repo.get_user_profile_by_username(username)
     
-    async def get_account(self, user_id: UUID) -> UserProfileResponse:
+    async def get_account(self, user_id: UUID) -> UserAccountResponse:
         return await self.user_repo.get_user_account(user_id)
+    
+    async def update_account(
+        self,
+        account_id: UUID,
+        data: UserUpdateRequest
+    ) -> UserAccountResponse:
+        return await self.user_repo.update_user(account_id, data)
